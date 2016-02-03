5ee42ff37fc514a0367a21ac42584996a8ee700e
kurone <kurone@cbase.co.jp>
2016-02-03 15:34:46 +0900

    first commit

---
+++ Fuga.php
@@ -0,0 +1,20 @@
+<?PHP
+/**
+ * @category Fuga
+ * @author   mkurone
+ *
+ */
+
+
+
+
+class Fuga {
+
+    public function index() {
+        return true;
+    }
+
+	public function index2() {
+		return false;
+	}
+}
